b758080047a02bafe2eca84292e80070a4bef059
victor90braz <victor.90braz@gmail.com>
2023-08-17 21:47:40 +0200

    Create initialUserState

--- src/store/slice/slice.ts
+++ src/store/slice/slice.ts
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../data/interface/interface";
+
+const initialUserState: User = {
+	id: "",
+	name: "",
+	email: "",
+	github: "",
+};
 
 export const usersSlice = createSlice({
-    name:"users",
-    initialState:???,
-    reducers:{}
-})
+	name: "users",
+	initialState: initialUserState,
+	reducers: {},
+});
