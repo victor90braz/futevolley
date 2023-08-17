71be1fbbfb55bfcc7a8a5a437713133b6d11a0f4
victor90braz <victor.90braz@gmail.com>
2023-08-17 22:09:56 +0200

    Create users in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./Slice/slice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		users: usersReducer,
+	},
 });
