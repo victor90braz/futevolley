ad53b2dfb3aabfea6ffe4265a5c97a3e30694cf6
victor90braz <victor.90braz@gmail.com>
2023-08-18 09:47:10 +0200

    Add callback to initialState

--- src/store/slice/slice.ts
+++ src/store/slice/slice.ts
@@ -1,11 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import userTemplate from "../../data/userTemplate";
 
-const initialState = () => {
+const initialState = (() => {
 	const persistanceState = localStorage.getItem("__redux__state__");
 
 	return persistanceState ? JSON.parse(persistanceState).users : userTemplate;
-};
+})();
 
 export const usersSlice = createSlice({
 	name: "users",
